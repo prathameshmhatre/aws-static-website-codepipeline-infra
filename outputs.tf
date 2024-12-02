3ed836465030a5a1079d8bc269a5835fd97e253d
Prathamesh Mhatre <prmha@nhsbsa.nhs.uk>
2024-12-02 19:37:51 +0530

    Complete s3bucket resource creation for static hosting

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "dev_website_url" {
+  value       = module.dev.dev_website_url
+  description = "URL for the dev environment static website"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region for the resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS profile for the resources"
+  type        = string
+  default     = "default"
+}
